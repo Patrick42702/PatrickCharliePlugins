2e926eb4e80c51bf7a364467aa6f422143c89b15
Patrick42702 <patrick42702@yahoo.com>
2026-03-11 08:32:24 -0400

    Added support for IAdvisor plugin
    Implemented IAdvisor plugin using the basic strategy in
    patrick.client.Advisor.java

---
+++ src/main/java/patrick/client/Advisor.java
@@ -0,0 +1,12 @@
+package patrick.client;
+
+import charlie.card.Card;
+import charlie.card.Hand;
+import charlie.plugin.IAdvisor;
+import charlie.util.Play;
+
+public class Advisor implements IAdvisor {
+
+  BasicStrategy bs = new BasicStrategy();
+  public Play advise(Hand hand, Card upCard) { return bs.getPlay(hand, upCard); }
+}
